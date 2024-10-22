710714ca03c0dc5d035199ec52f0f57a04b64515
MW <myw.song@gmail.com>
2024-10-23 04:03:38 +0900

    feat: 상품관련완성

---
+++ omiyami-jsp/src/main/java/com/omiyami/shop/product/review/ReviewVO.java
@@ -0,0 +1,52 @@
+package com.omiyami.shop.product.review;
+
+import java.sql.Timestamp;
+
+public class ReviewVO {
+
+	private int user_id;
+	private String username;
+    private int product_id;
+    private String status;
+    private String content;
+    private Timestamp write_date;
+    
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(Timestamp write_date) {
+		this.write_date = write_date;
+	}
+	
+}
